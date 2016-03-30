8429d3a2f0d3690a2ba5028f3f0774cdadf8a984
minaXU <812448369@qq.com>
2016-03-30 09:12:40 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by xuNick on 16/3/29.
+//  Copyright © 2016年 NN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
